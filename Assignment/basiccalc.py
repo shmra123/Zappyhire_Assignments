49180a5399d52d9f2df040fdb84b3676f176626f
recipie-css-html-js <126555986+shmra123@users.noreply.github.com>
2024-02-01 15:08:42 +0530

    Add files via upload

---
+++ Assignment/basiccalc.py
@@ -0,0 +1,15 @@
+a=int(input("enter first no"))
+b=int(input("enter second  no"))
+y=input("enter the operator to be performed")
+if(y=="+"):
+    c=a+b
+    print(c)
+if(y=="-"):
+    c=a-b
+    print(c)
+if(y=="*"):
+    c=a*b
+    print(c)
+if(y=="/"):
+    c=a/b
+    print(c)
